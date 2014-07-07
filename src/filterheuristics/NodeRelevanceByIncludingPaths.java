e52ceb877abbedd5e52b4f78c2478baa2b46744f
jensschn <jensschn@5dec2bdd-bc6e-6ca5-d606-a3cd11ca8a0f>
2014-07-07 13:46:12 +0000

    Added caches to commit

--- src/filterheuristics/NodeRelevanceByIncludingPaths.java
+++ src/filterheuristics/NodeRelevanceByIncludingPaths.java
@@ -6,15 +6,18 @@ import graph.GraphCleaner.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 import org.graphstream.graph.Node;
 
 public class NodeRelevanceByIncludingPaths {
 	
 	private Map<String, Integer> numberOfIncludingPaths;
-
+	private Map<String,String> visitedWhileClustering;
 	
 	
 	public NodeRelevanceByIncludingPaths(){
@@ -29,6 +32,77 @@ public class NodeRelevanceByIncludingPaths {
 	public void filterTheNMostVisited(WTPGraph graph, List<Path> paths, int maxNumber){
 		generateIncludingPathMap(paths);
 		findNMostRelevant(graph, maxNumber);
+		//filerBiggestCluster(graph);
+	}
+
+	private void filerBiggestCluster(WTPGraph graph) {
+		visitedWhileClustering = new HashMap<String, String>();
+		List<List<String>> clusterList = new LinkedList<List<String>>();
+		for(Node n : graph.getGraph().getNodeSet()){
+			String id = n.getId();
+			if(visitedWhileClustering.get(id) == null){
+				clusterList.add(getCluster(graph, id));
+			}
+		}
+		//print Clusters
+		int number = 1;
+		System.out.println();
+		System.out.println("Clusters");
+		for(List<String> lll : clusterList){
+			System.out.println("|||| Cluster#:"+number++);
+			for(String st : lll){
+				System.out.println(st);
+			}
+			System.out.println();
+			System.out.println();
+		}
+		
+		
+		//get size of biggest cluster;
+		int biggestSize = 0;
+		for(List<String> list : clusterList){
+			if(list.size() > biggestSize)
+				biggestSize = list.size();
+		}
+		Map<String,String> survivingNodeIds = new HashMap<String, String>();
+		for(List<String> list : clusterList){
+			if(list.size() == biggestSize){
+				for(String idL : list)
+					survivingNodeIds.put(idL, idL);
+			}
+		}
+
+		//generate deleting queue
+		Queue<String> deleteQueue = new LinkedList<String>();
+
+		for(Node node : graph.getGraph().getNodeSet()){
+			if(survivingNodeIds.get(node.getId())==null){
+				deleteQueue.add(node.getId());
+			}
+		}
+		
+		//remove dead Nodes
+		while(!deleteQueue.isEmpty()){
+			graph.getGraph().removeNode(deleteQueue.poll());
+		}
+	}
+
+	private List<String> getCluster(WTPGraph graph, String id) {
+		Queue<String> q = new LinkedList<String>();
+		List<String> list = new LinkedList<String>();
+		q.add(id);
+		while(!q.isEmpty()){
+			String temp = q.poll();
+			if(visitedWhileClustering.get(temp)==null){
+				visitedWhileClustering.put(temp,temp);
+				list.add(temp);
+				Iterator<Node> neighbourNodesIterator = graph.getGraph().getNode(temp).getNeighborNodeIterator();
+				while(neighbourNodesIterator.hasNext()){
+					q.add(neighbourNodesIterator.next().getId());
+				}
+			}
+		}
+		return list;
 	}
 
 	private void findNMostRelevant(WTPGraph graph, int maxNumber) {
